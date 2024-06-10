7ba6dcd471fc596b65d1c5c987249e422b1290ba
Ignacio Maria <ignacio.maria1994@gmail.com>
2024-06-10 11:24:31 -0300

    Layout

---
+++ src/UI/Page/index.jsx
@@ -0,0 +1,11 @@
+import Header from "../../Header";
+
+export default function Page(props){
+    return(
+        <>  
+            <Header/>
+            <main><h1>Main</h1></main>
+            
+        </>
+    );
+}
