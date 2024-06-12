4014e36e974dea5ea3a162249367f7d5193810de
maxiderecho <maxi.derecho@gmail.com>
2024-06-12 11:53:14 -0300

    hotfix ubicación de footer y header

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from 'react-redux'
 import { store,persistor } from './Redux/store.js'
 import { BrowserRouter } from 'react-router-dom'
 import { PersistGate } from 'redux-persist/integration/react'
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
