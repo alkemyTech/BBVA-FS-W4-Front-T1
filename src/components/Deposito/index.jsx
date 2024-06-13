35bcd00b4b52697116f8ead8cd722a9ca0d4e880
Alan Ades <alan.ades2@gmail.com>
2024-06-13 15:10:19 -0300

    Borrando instrascendencias

--- src/components/Deposito/index.jsx
+++ src/components/Deposito/index.jsx
@@ -1,4 +1,3 @@
-// components/Deposito.js
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import {
@@ -6,8 +5,7 @@ import {
   Button,
   Box,
   MenuItem,
-  Typography,
-  CircularProgress,
+  Typography
 } from '@mui/material';
 import { deposit } from '../../api/Transaction';
 import MySnackbar from '../../UI/MySnackBar';
