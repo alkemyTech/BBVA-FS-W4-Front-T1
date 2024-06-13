c73cf0c8f5e0a5d4822500f7af8cada5412f3414
Alan Ades <alan.ades2@gmail.com>
2024-06-13 15:11:51 -0300

    Borrando intrascendencias 2

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,4 +1,3 @@
-// redux/store.js
 import { configureStore } from '@reduxjs/toolkit';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
