28cf3d7a06bad479537b83038091b9037e80f84e
maxiderecho <maxi.derecho@gmail.com>
2024-06-19 10:15:37 -0300

    Agrego class container

--- src/components/Transferencia/index.jsx
+++ src/components/Transferencia/index.jsx
@@ -1,17 +1,21 @@
-import { Grid, Typography } from '@mui/material';
-import React from 'react';
+import { Container, Grid, Typography } from "@mui/material";
+import React from "react";
 import { useLocation } from "react-router";
-import ArrowBackComponent from '../../UI/ArrowBack';
+import ArrowBackComponent from "../../UI/ArrowBack";
+
 const Transferencia = () => {
-    const location = useLocation();
-    const { account } = location.state || {};
-    console.log("Transferencia account",account);
-    return (
-        <Grid container>
-            <ArrowBackComponent/>   
-           <Typography> Transferencia</Typography>
-        </Grid>
-    );
-}
+  const location = useLocation();
+  const { account } = location.state || {};
+  console.log("Transferencia account", account);
+
+  return (
+    <Grid container >
+      <Grid container className="container">
+        <ArrowBackComponent />
+        <Typography> Transferencia</Typography>
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Transferencia;
