3804747e82ec8ca725f174f907d673925444cdc5
janarozen <rozenbergjana@gmail.com>
2024-06-26 12:27:47 -0300

    se agrego la nevegacion de ver movimientos en cuentas, reutlizando mismo componente

--- src/components/Account/AccountDetails.jsx
+++ src/components/Account/AccountDetails.jsx
@@ -1,133 +1,170 @@
-import React, { useState } from "react";
+import React from 'react';
 import {
   Card,
   CardContent,
   Grid,
   Typography,
   Box,
-  Divider,
   Accordion,
   AccordionSummary,
   AccordionDetails,
   Button,
-} from "@mui/material";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import PropTypes from "prop-types";
-import { useNavigate } from "react-router";
+} from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import PropTypes from 'prop-types';
+import { useNavigate } from 'react-router';
 
-const AccountDetailsCard = ({ account }) => {
+const AccountDetailsCard = ({ account, showVerMovimientos }) => {
   const formatCurrency = (amount, currency) => {
-    return new Intl.NumberFormat("es-AR", {
-      style: "currency",
+    return new Intl.NumberFormat('es-AR', {
+      style: 'currency',
       currency,
     }).format(amount);
   };
+
   const navigate2 = useNavigate();
 
   const handleCardClick = (path, account) => {
-    console.log("Transferir", account);
+    console.log('Transferir', account);
     navigate2(path, { state: { account } });
+  };
+
+  const handleCardClick2 = (path, account) => {
+    console.log('VER MOVIMIENTOS', showVerMovimientos);
+    navigate2(path, { state: { account, booleanProp: true } });
   };
 
   return (
     <Grid item xs={12}>
       <Card
         sx={{
-          backgroundColor: "#f1f6f5",
-          color: "#000000",
-          borderRadius: "2vh",
-          padding: "1vh",
-          boxShadow: "0 8px 16px rgba(0, 0, 0, 0.2)",
-          display:"centre"
+          backgroundColor: '#ffffff',
+          color: '#000000',
+          borderRadius: '2vh',
+          padding: '1vh',
+          boxShadow: '0 8px 16px rgba(0, 0, 0, 0.2)',
         }}
       >
         <CardContent>
           <Grid container>
-            <Grid item xs={12}sm={10}>
-          <Typography
-            variant="subtitle"
-            component="div"
-            sx={{ marginBottom: "8px", color: "#472183" }}
-          >
-            {account.accountType === "CAJA_AHORRO"
-              ? "Caja de Ahorro"
-              : "Cuenta Corriente"}
-          </Typography>
-          <Typography variant="h4" component="div" color="#000000">
-            {formatCurrency(account.balance, account.currency)}
-            {account.currency}
-          </Typography>
-          <Typography
-            variant="body2"
-            component="div"
-            sx={{ marginBottom: "0vh", color: "#4b56d2" }}
-          >
-            Balance disponible (a partir de hoy)
-          </Typography>
-          </Grid>
-          <Grid item xs={12} sm={2}>
-          <Button
-            variant="contained"
-            sx={{
-              marginBottom: "2vh",
-              color: "#ffffff",
-              backgroundColor: "#656ED4",
-              border: "1px solid #656ED4",
-              padding: "1vh",
-              maxWidth: "14vh",
-              "&:hover": {
-                backgroundColor: "#3c4370", // Color oscuro al pasar el mouse
-                borderColor: "#3c4370", // Borde oscuro al pasar el mouse
-              },
-            }}
-            onClick={() => handleCardClick("/transferencia", account)}
-          >
-            <b>Transferir</b>
-          </Button>
-          </Grid>
-
-          <Grid item xs={12}>
-          <Accordion
-            sx={{
-              boxShadow: "none",
-              borderTop: "none",
-              backgroundColor: "transparent",
-              "&:before": { display: "none" },
-            }}
-            disableGutters
-          >
-            <AccordionSummary
-              expandIcon={<ExpandMoreIcon />}
-              sx={{ padding: 0 }} // Agrega esta línea para eliminar el padding
-            >
-              <Typography sx={{ color: "#609AF6" }}>
-                Ver detalles de la cuenta
+            <Grid item xs={12} sm={10}>
+              <Typography
+                variant="subtitle"
+                component="div"
+                sx={{ marginBottom: '8px', color: '#472183' }}
+              >
+                {account.accountType === 'CAJA_AHORRO' ? 'Caja de Ahorro' : 'Cuenta Corriente'}
               </Typography>
-            </AccordionSummary>
-            <AccordionDetails sx={{ padding: 0 }}>
-              <Box>
-                <Typography variant="subtitle1" component="div" color="#609AF6">
-                  Alias:
-                </Typography>
-                <Typography variant="body1" component="div" color="#000000">
-                  {account.alias}
-                </Typography>
-                <Typography variant="subtitle1" component="div" color="#609AF6">
-                  CBU:
-                </Typography>
-                <Typography variant="body1" component="div" color="#000000">
-                  {account.cbu}
-                </Typography>
-                <Typography variant="subtitle1" component="div" color="#609AF6">
-                  Límite de Transacción:
-                </Typography>
-                <Typography variant="body1" component="div" color="#000000">
-                  {formatCurrency(account.transactionLimit, account.currency)}
-                </Typography>
-              </Box>
-            </AccordionDetails>
-          </Accordion>
-          </Grid>
+              <Typography variant="h4" component="div" color="#000000">
+                {formatCurrency(account.balance, account.currency)}
+                {account.currency}
+              </Typography>
+              <Typography
+                variant="body2"
+                component="div"
+                sx={{ marginBottom: '0vh', color: '#4b56d2' }}
+              >
+                Balance disponible (a partir de hoy)
+              </Typography>
+            </Grid>
+            <Grid item xs={12} sm={2} container direction="column" spacing={2}>
+              <Grid item>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  sx={{
+                    color: '#ffffff',
+                    backgroundColor: '#656ED4',
+                    border: '1px solid #656ED4',
+                    padding: '1vh',
+                    '&:hover': {
+                      backgroundColor: '#3c4370', // Color oscuro al pasar el mouse
+                      borderColor: '#3c4370', // Borde oscuro al pasar el mouse
+                    },
+                  }}
+                  onClick={() => handleCardClick('/transferencia', account)}
+                >
+                  <b>Transferir</b>
+                </Button>
+              </Grid>
+              { showVerMovimientos && <Grid item>
+                <Button
+                  variant="contained"
+                  fullWidth
+                  disabled={!showVerMovimientos} // Deshabilitar el botón si showVerMovimientos es false
+                  sx={{
+                    color: '#ffffff',
+                    backgroundColor: '#656ED4',
+                    border: '1px solid #656ED4',
+                    padding: '1vh',
+                    '&:hover': {
+                      backgroundColor: '#3c4370', // Color oscuro al pasar el mouse
+                      borderColor: '#3c4370', // Borde oscuro al pasar el mouse
+                    },
+                  }}
+                  onClick={() => handleCardClick2('/account', account)}
+                >
+                  <b>Ver movimientos</b>
+                </Button>
+              </Grid>}
+            </Grid>
+            <Grid item xs={12}>
+              <Accordion
+                sx={{
+                  boxShadow: 'none',
+                  borderTop: 'none',
+                  backgroundColor: 'transparent',
+                  '&:before': { display: 'none' },
+                }}
+                disableGutters
+              >
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  sx={{ padding: 0 }} // Agrega esta línea para eliminar el padding
+                >
+                  <Typography sx={{ color: '#609AF6' }}>
+                    Ver detalles de la cuenta
+                  </Typography>
+                </AccordionSummary>
+                <AccordionDetails sx={{ padding: 0 }}>
+                  <Box>
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      color="#609AF6"
+                    >
+                      Alias:
+                    </Typography>
+                    <Typography variant="body1" component="div" color="#000000">
+                      {account.alias}
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      color="#609AF6"
+                    >
+                      CBU:
+                    </Typography>
+                    <Typography variant="body1" component="div" color="#000000">
+                      {account.cbu}
+                    </Typography>
+                    <Typography
+                      variant="subtitle1"
+                      component="div"
+                      color="#609AF6"
+                    >
+                      Límite de Transacción:
+                    </Typography>
+                    <Typography variant="body1" component="div" color="#000000">
+                      {formatCurrency(
+                        account.transactionLimit,
+                        account.currency
+                      )}
+                    </Typography>
+                  </Box>
+                </AccordionDetails>
+              </Accordion>
+            </Grid>
           </Grid>
         </CardContent>
       </Card>
@@ -144,6 +181,7 @@ AccountDetailsCard.propTypes = {
     currency: PropTypes.string.isRequired,
     transactionLimit: PropTypes.number.isRequired,
   }).isRequired,
+  showVerMovimientos: PropTypes.bool.isRequired, // Añadir esta línea
 };
 
 export default AccountDetailsCard;
