f1716da02d15e1188d48334a7633104ec4c36129
maxiderecho <maxi.derecho@gmail.com>
2024-07-01 19:02:52 -0300

    cambio color en flechas banner

--- src/components/Home/Banner/index.jsx
+++ src/components/Home/Banner/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Slider from "react-slick";
 import { Typography, Grid, Link, Fade } from "@mui/material";
+import "./index.css";
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
